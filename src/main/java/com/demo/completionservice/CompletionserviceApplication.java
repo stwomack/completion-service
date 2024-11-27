fc3be959dcc7344261a5a78bc6533de4e98715d5
Steve Womack <swomack@mbp15-home.local>
2024-11-27 13:28:17 -0600

    Adds files

---
+++ src/main/java/com/demo/completionservice/CompletionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.completionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletionserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/completionservice/CompletionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.completionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
